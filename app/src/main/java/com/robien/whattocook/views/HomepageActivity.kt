77af9e4707236a4c124b5d93c5813336471bad9b
Rob Marquez <robienallen@yahoo.com>
2020-06-04 20:25:45 -0700

    Addedsearch bar at top of the page

--- app/src/main/java/com/robien/whattocook/views/HomepageActivity.kt
+++ app/src/main/java/com/robien/whattocook/views/HomepageActivity.kt
@@ -2,6 +2,8 @@ package com.robien.whattocook.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SearchView
+import android.widget.TextView
 import com.robien.whattocook.R
 
 class HomepageActivity : AppCompatActivity() {
@@ -9,5 +11,9 @@ class HomepageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_homepage)
+
+        // Get references to widgets
+        var searchView = findViewById<SearchView>(R.id.ingredientSearchView)
+        var noRecipesTextView = findViewById<TextView>(R.id.emptyTextView)
     }
 }
